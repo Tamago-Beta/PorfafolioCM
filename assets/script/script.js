d4efb0c998d30d2032f9938bc82518b79fd96c5b
CarlosMacvas <charly_mv@outlook.com>
2023-03-11 11:01:37 -0500

    Version 2 usando la metodologia Atomic Design

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,13 @@
-const barras = document.querySelector(".cabecera__barmenu");
-const menu = document.querySelector(".cabecera__menu");
+import { handlesubmit } from "./formulario.js";
+import { toggleMenu, linkAction } from "./barrasMenu.js";
 
-var line1__bars = document.querySelector(".cabecera__barmenu__line1");
-var line2__bars = document.querySelector(".cabecera__barmenu__line2");
-var line3__bars = document.querySelector(".cabecera__barmenu__line3"); 
+// ==================== DECLARACION DE VARIALBES ====================
+const barras = document.querySelector(".barras");
+const contactameFormulario = document.querySelector('#form');
+const navLink = document.querySelectorAll('.menu__link');
 
-barras.addEventListener("click", () =>{
-    menu.classList.toggle('cabecera__menu--activo');
+// ============================================================
 
-    line1__bars.classList.toggle('cabecera__barmenu__line-1');
-    line2__bars.classList.toggle('cabecera__barmenu__line-2');
-    line3__bars.classList.toggle('cabecera__barmenu__line-3');
-})
-
-
+barras.addEventListener('click', toggleMenu);
+contactameFormulario.addEventListener('submit', handlesubmit)
+navLink.forEach(n => n.addEventListener('click', linkAction));
